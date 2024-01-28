4ab967fe4e097ff4944ec0446c1dd09fc5f8d8e3
guramxatiashvili <guramxatiashvili@hotmail.com>
2024-01-28 19:43:46 +0000

    write Rock-Paper-Scissor logic

--- treasure.py
+++ treasure.py
@@ -7,7 +7,7 @@ position = input() # Where do you want to put the treasure?
 
 letter=position[0].lower() #get only letter
 abc = ["a", "b", "c"]
-letter_index=abc.index(letter) #if letter == b index == 1, if a index ==0
+letter_index=abc.index(letter) #if letter == b, index == 1, if a,index ==0
 number_index=int(position[1])-1 # position 3 is [2], so 3-1
 
 map[number_index][letter_index]='x'
