6106f93bb02d366ea7727f457c3116cf975accf1
guramxatiashvili <guramxatiashvili@hotmail.com>
2024-01-28 18:59:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+
+
