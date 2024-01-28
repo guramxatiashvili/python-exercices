28b6a2382d4b8c9600cd7c93f3448dcc2d702e44
guramxatiashvili <guramxatiashvili@hotmail.com>
2024-01-28 19:02:01 +0000

    create nested lists and choose position of X

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ map = [line1, line2, line3]
 print("Hiding your treasure! X marks the spot.")
 position = input() # Where do you want to put the treasure?
 
+letter=position[0].lower() #get only letter
+abc = ["a", "b", "c"]
+letter_index=abc.index(letter) #if letter == b index == 1, if a index ==0
+number_index=int(position[1])-1 # position 3 is [2], so 3-1
 
+map[number_index][letter_index]='x'
+
+print(f"{line1}\n{line2}\n{line3}")
