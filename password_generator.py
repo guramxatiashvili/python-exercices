bdf94440d1c686222378271640cc3c56379f1a3d
guramxatiashvili <guramxatiashvili@hotmail.com>
2024-01-29 11:19:15 +0000

    shuffle generated password to randomize order

--- password_generator.py
+++ password_generator.py
@@ -1,24 +1,33 @@
 #Password Generator Project
 import random
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Order not randomised:
-generated_password=""
-for letter in range(0,nr_letters):
-  letter=random.choice(letters)
-  generated_password+=letter
-for symbol in range(0,nr_symbols):
-  symbol=random.choice(symbols)
-  generated_password+=symbol
-for number in range(0,nr_numbers):
-  number=random.choice(numbers)
-  generated_password+=number
+generated_password = ""
+for letter in range(0, nr_letters):
+  letter = random.choice(letters)
+  generated_password += letter
+for symbol in range(0, nr_symbols):
+  symbol = random.choice(symbols)
+  generated_password += symbol
+for number in range(0, nr_numbers):
+  number = random.choice(numbers)
+  generated_password += number
 print(generated_password)
+
+#Order randomised:
+shuffled = ''.join(random.sample(generated_password, len(generated_password)))
+print(shuffled)
